34403f05a5dab6983bf062d2ca7b569636d2ef14
nhat.vo <nhatvo31415@gmail.com>
2021-08-04 15:35:49 -0700

    update bot.py

--- ask-dota/bot.py
+++ ask-dota/bot.py
@@ -6,6 +6,8 @@ import random
 import requests
 import json
 
+from requests.models import Response
+
 #pass dota hero list dict
 dict = herolist.dotadict
 
@@ -20,7 +22,7 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 
 bot = discord.Client()
 
-#BOT PREFIX
+### PREFIX ###
 bot = commands.Bot(command_prefix='!#')
 
 #succesfully message
@@ -28,7 +30,7 @@ bot = commands.Bot(command_prefix='!#')
 async def on_ready():
     print(f'{bot.user} has connected to Discord!')
 
-### FUNCTION ###
+### CALL ###
 #!info
 @bot.command(name='info', help='bot info')
 async def test(ctx):
@@ -99,4 +101,33 @@ async def test(ctx, steamid3):
                 response = f'{response}, {dict[match[item]]}'
     await ctx.send(response)
 
+#! most picked hero last 40 games
+@bot.command(name='mostpicklast40')
+async def test(ctx, steamid3):
+    odPath = f"https://api.opendota.com/api/players/{steamid3}/matches?limit=40"
+    r = requests.get(odPath)
+    response_info = json.loads(r.content)
+    response = ''
+    pickedlist = []
+    for match in response_info:
+        for item in match:
+            if item=='hero_id':
+                hero = dict[match[item]]
+                pickedlist.append(hero)
+    hero, counts = most_frequent(pickedlist)
+    response = f'Player picked {hero} for {counts} times'
+    await ctx.send(response)
+
+#FUNCTIONS
+def most_frequent(List):
+    counter = 0
+    num = List[0]
+     
+    for i in List:
+        curr_frequency = List.count(i)
+        if(curr_frequency> counter):
+            counter = curr_frequency
+            num = i
+ 
+    return num, curr_frequency
 bot.run(TOKEN)
