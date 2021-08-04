9d0a808b6f6fcf4ada6f767acb48b966fdca0367
nhat.vo <nhatvo31415@gmail.com>
2021-08-04 09:18:36 -0700

    a

--- ask-dota/bot.py
+++ ask-dota/bot.py
@@ -1,16 +1,37 @@
 # bot.py
 import os
-
 import discord
+
+#import
+from discord.ext import commands
 from dotenv import load_dotenv
 
+#connect discord ToKen
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
+#BOT PREFIX
+bot = commands.Bot(command_prefix='!#')
+
 client = discord.Client()
 
+#succesfully message
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
+
+### FUNCTION ###
+#!info
+@bot.command(name='info', help='bot info')
+async def test(ctx):
+    response = 'ASK-DOTA BOT'
+    await ctx.send(response)
+
+
+
+
+
+
+
 
 client.run(TOKEN)
