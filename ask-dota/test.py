5f055b214a3cb9386c81c54234d6f39afad9e8e2
nhat.vo <nhatvo31415@gmail.com>
2021-08-05 15:48:52 -0700

    test on embed

--- ask-dota/test.py
+++ ask-dota/test.py
@@ -22,7 +22,7 @@ def most_frequent(List):
         if(curr_frequency> counter):
             counter = curr_frequency
             num = i
-    return num, curr_frequency
+    return num, counter
 
 def most_frequent1(List):
     counter = 0
@@ -35,15 +35,9 @@ def most_frequent1(List):
             
     return hero, counter
 
-
-
-
-
 list = ['Slardar', 'Legion Commander', 'Mars', 'Slardar', 'Grimstroke', 'Tusk', 'Grimstroke', 'Grimstroke', 'Earth Spirit', 'Underlord', 'Tusk', 'Lion', 'Tusk', 'Clockwerk', 'Viper', 'Invoker', 'Disruptor', 
 'Dragon Knight', 'Silencer', 'Tusk', 'Viper', 'Jakiro', 'Disruptor', 'Crystal Maiden', 'Medusa', 'Dragon Knight', 'Drow Ranger', 'Slardar', 'Invoker', 'Mars', 'Terrorblade', 'Centaur Warrunner', 'Luna', 'Invoker', 'Underlord', 'Underlord', 'Centaur Warrunner', 'Tusk', 'Tusk', 'Vengeful Spirit']
-print(most_frequent1(list))
-
-
+#print(most_frequent1(list))
 
 
 '''
@@ -65,12 +59,7 @@ print(pickedlist)
 print(most_frequent(pickedlist))
 
 '''
-
-
 #print(len(herolist.dotadict))
-
-
-
 
 #random a hero from herolist
 num = randint(0, len(dict))
@@ -78,3 +67,21 @@ num = randint(0, len(dict))
 response = dict[num]
 #print(response)
 
+
+######### EMBEDED #######################
+#f to catch player info from opendata api
+def playerinfo(steamid32):
+    odPath = f'https://api.opendota.com/api/players/{steamid32}'
+    r = requests.get(odPath)
+    content_jason = json.loads(r.content)
+    account_id= f"{content_jason['profile']['account_id']}"
+    personaname = f"{content_jason['profile']['personaname']}"
+    avatarfull = f"{content_jason['profile']['avatarfull']}"
+    mmr_estimate = f"{content_jason['mmr_estimate']['estimate']}"
+    competitive_rank = f"{content_jason['competitive_rank']}"
+
+
+
+    return account_id, personaname, avatarfull, mmr_estimate, competitive_rank
+
+print(playerinfo(56091566))
